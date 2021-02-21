f0db33fed0955f05be7c7fd65b792ee37b89d238
esra akat <383196@ogr.ktu.edu.tr>
2021-02-21 16:49:34 +0300

    15.02.2021

---
+++ Generics/MyList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        //why is it outside of method? Because we defined an array what gets all elements of MyList class.
+        T[] items;
+
+        //ctor - constructor is a method what runs when we use new operator.
+        public MyList()
+        {
+            //we create 0 element an array. 
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            //tempArray - it keeps refernce numbers why they do not get lost.
+            T[] tempArray = items;
+            items = new T[items.Length + 1]; //element numbers for array
+
+            //elements of tempArray------>items
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+
+        public int Count  //propfull
+        {
+            get { return items.Length; } //number of array
+        }
+
+    }
+}
+//generic class is a special type
+//T - class type
+//MyList uses an array in the background
+//When we use new operator, it get a new reference number and deletes the old one.
