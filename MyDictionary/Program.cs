19af36a195d52d287e99f8b788c51c0be366ceb5
Esra Akat <383196@ogr.ktu.edu.tr>
2022-02-08 01:40:57 +0300

    8 Şubat 2022

--- MyDictionary/Program.cs
+++ MyDictionary/Program.cs
@@ -6,10 +6,12 @@ namespace MyDictionary
     {
         static void Main(string[] args)
         {
-            
-
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+            myDictionary.Add("Esra", 21);
+            myDictionary.Add("Ferhat", 19);
+            myDictionary.Add("Yusuf", 17);
+            myDictionary.Add("Eylem", 15);
+            Console.WriteLine(myDictionary.Count);
         }
     }
-
-
 }
