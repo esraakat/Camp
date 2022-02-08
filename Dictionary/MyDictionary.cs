6f2563e1eec7e8752d05d5fa8b21022a3a8f0c7e
Esra Akat <383196@ogr.ktu.edu.tr>
2022-02-09 00:02:52 +0300

    9 Şubat 2022

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class MyDictionary<nT, aT>
+    {
+        nT[] names;
+        aT[] ages;
+
+        public MyDictionary()
+        {
+            names = new nT[0];
+            ages = new aT[0];
+        }
+
+        public int Count()
+        {
+            return names.Length;
+        }
+
+        public void Add(nT name, aT age)
+        {
+            nT[] tempNames = names;
+
+            names = new nT[names.Length + 1];
+
+            for (int i = 0; i < tempNames.Length; i++)
+            {
+                names[i] = tempNames[i];
+            }
+            names[names.Length - 1] = name;
+
+            aT[] tempAges = ages;
+
+            ages = new aT[ages.Length + 1];
+
+            for (int j = 0; j < tempAges.Length; j++)
+            {
+                ages[j] = tempAges[j];
+            }
+
+            ages[ages.Length - 1] = age;
+
+            Console.WriteLine(name + " " + age);
+        }
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> family = new MyDictionary<string, int>();
+            family.Add("Ayhan", 43);
+            family.Add("Halime", 42);
+            family.Add("Esra", 21);
+            family.Add("Ferhat", 19);
+            Console.WriteLine(family.Count()); 
+        }
+    }
+}
