9255b3ebbf9deb705907fa576d44ed4250f59a90
Esra Akat <383196@ogr.ktu.edu.tr>
2022-02-26 01:03:43 +0300

    26.02.2022

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -8,8 +8,8 @@ namespace Linq
         {
             List<Category> categories = new List<Category>
             {
-                new Category {CategoryId = 1, CategoryName = "Bilgisayar" },
-                new Category {CategoryId = 2, CategoryName = "Telefon"},
+                new Category {CategoryId = 1, CategoryName = "Computer" },
+                new Category {CategoryId = 2, CategoryName = "Phone"},
             };
 
             List<Product> products = new List<Product>
@@ -17,15 +17,78 @@ namespace Linq
                 new Product {Id = 1, CategoryId = 1, ProductName = "Acer Laptop", QuantityPerUnit = "32 GB Ram", UnitPrice = 10000, UnitsInStock = 5},
                 new Product {Id = 2, CategoryId = 1, ProductName = "Asus Laptop", QuantityPerUnit = "16 GB Ram", UnitPrice = 8000, UnitsInStock = 3},
                 new Product {Id = 3, CategoryId = 1, ProductName = "Hp Laptop", QuantityPerUnit = "8 GB Ram", UnitPrice = 10000, UnitsInStock = 2},
-                new Product {Id = 4, CategoryId = 2, ProductName = "Samsung Laptop", QuantityPerUnit = "4 GB Ram", UnitPrice = 6000, UnitsInStock = 15},
-                new Product {Id = 5, CategoryId = 2, ProductName = "Apple Laptop", QuantityPerUnit = "4 GB Ram", UnitPrice = 8000, UnitsInStock = 0},
+                new Product {Id = 4, CategoryId = 2, ProductName = "Samsung Telefon", QuantityPerUnit = "4 GB Ram", UnitPrice = 6000, UnitsInStock = 15},
+                new Product {Id = 5, CategoryId = 2, ProductName = "Apple Telefon", QuantityPerUnit = "4 GB Ram", UnitPrice = 8000, UnitsInStock = 0},
             };
 
+            //Single Line Query
+            Test(products);
+            GetProducts(products);
+            AnyTest(products);
+            FindTest(products);
+            FindAllTest(products);
+            AscDescTest(products);
+            ClassicLinqTest(products);
+            JoinTest(categories, products);
+        }
+
+        private static void JoinTest(List<Category> categories, List<Product> products)
+        {
+            var result = from p in products
+                         join c in categories
+                         on p.CategoryId equals c.CategoryId
+                         where p.UnitPrice > 6000
+                         orderby p.UnitPrice ascending
+                         select new ProductDto { Id = p.Id, CategoryName = c.CategoryName, ProductName = p.ProductName, UnitPrice = p.UnitPrice };
+
+            foreach (var productDto in result)
+            {
+                Console.WriteLine("{0}---------{1}", productDto.ProductName, productDto.CategoryName); //string interpolation
+            }
+        }
+
+        private static void ClassicLinqTest(List<Product> products)
+        {
+            var result = from p in products
+                         where p.UnitPrice > 6000
+                         orderby p.UnitPrice descending, p.ProductName ascending
+                         select new ProductDto { Id = p.Id, ProductName = p.ProductName, UnitPrice = p.UnitPrice };
+        }
+
+        private static void AscDescTest(List<Product> products)
+        {
+            var result = products.Where(p => p.ProductName.Contains("top")).OrderByDescending(p => p.UnitPrice).ThenBy(p => p.ProductName);
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void FindAllTest(List<Product> products)
+        {
+            var result = products.FindAll(p => p.ProductName.Contains("top"));
+            Console.WriteLine(result);
+        }
+
+        private static void FindTest(List<Product> products)
+        {
+            var result = products.Find(p => p.Id == 3); //Aradığımız kritere uygun nesnenin kendisini veriyor
+            Console.WriteLine(result);
+        }
+
+        private static void AnyTest(List<Product> products)
+        {
+            var result = products.Any(p => p.ProductName == "Acer Laptop");//Listenin içerisinde eleman var mı yok mu?
+            Console.WriteLine(result);
+        }
+
+        private static void Test(List<Product> products)
+        {
             Console.WriteLine("Linq'siz -----------------------------");
 
             foreach (var product in products)
             {
-                if (product.UnitPrice > 6000)
+                if (product.UnitPrice > 6000 & product.UnitsInStock > 3)
                 {
                     Console.WriteLine(product.ProductName);
                 }
@@ -33,12 +96,11 @@ namespace Linq
 
             Console.WriteLine("Linq-----------------------------------");
 
-            var result = products.Where(p => p.UnitPrice > 5000 & p.UnitsInStock > 3);
-            foreach (var product in products)
+            var result = products.Where(p => p.UnitPrice > 6000 & p.UnitsInStock > 3);
+            foreach (var product in result)
             {
                 Console.WriteLine(product.ProductName);
             }
-
         }
 
         static List<Product> GetProducts(List<Product> products)
@@ -49,7 +111,7 @@ namespace Linq
             {
                 if (product.UnitPrice > 5000)
                 {
-                    products.Add(product);
+                    filteredProducts.Add(product);
                 }
             }
             return filteredProducts;
@@ -59,8 +121,14 @@ namespace Linq
         {
             return products.Where(p => p.UnitPrice > 5000).ToList();
         }
+    }
 
-
+    class ProductDto
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
     }
     class Product
     {
@@ -75,5 +143,8 @@ namespace Linq
     {
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
-    }
+    }    
 }
+
+//Use generally find, findallor where, any in real project
+//dto
