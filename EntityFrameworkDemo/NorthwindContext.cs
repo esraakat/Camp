964f5aa8df6198ffaa438f6a0582dec11e8ef1ed
Esra Akat <383196@ogr.ktu.edu.tr>
2022-03-15 18:22:07 +0300

    15.03.22

---
+++ EntityFrameworkDemo/NorthwindContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    public class NorthwindContext : DbContext  
+    {
+        //uygulamadaki nesneleri ve veritabanındaki tabloları ilişkilendiricez
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) //veritabanını belirledik
+        {
+            optionsBuilder.UseSqlServer(@"Server = (localdb)\MSSQLLocalDB; Database = Northwind; Trusted_Connection = true ");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //.net içerisinde ADO.NET var. Veritsbsnıns bağlanmamızı , orada sorgu yazmamızı sağlayan bir kütüphane. Bununla yazdığımız kodlar yorucu
+            //O yuzden çeşitli ORM yapıları kullanırız - Object Relational Mapping
+            //Entity Framework bir orm - NHibernate onun bir alternatifidir.
+
+            GetAll();
+
+
+
+        }
+
+        private static void GetAll()
+        {
+            NorthwindContext context = new NorthwindContext();
+            foreach (var product in context.Products)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void GetProductsyCategory(int id)
+        {
+            NorthwindContext context = new NorthwindContext();
+
+            var result = context.Products.Where(p => p.CategoryId == id);
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+    }
+}
